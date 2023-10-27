69f5340f195a49a8d96ca162ae583f7dcdba7395
daratehexplorer <darinka.lipovac@gmail.com>
2023-10-27 14:04:10 +0100

    Finalise game code to keep score over multiple rounds

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let wantToPlay = prompt("Wanna play Rock, Paper, Scissors with me?", )
+        switch (wantToPlay.toLowerCase()) {
+        case "yes": 
+            console.log(game());
+        break;
+        case "no":
+            alert("You're no fun!")
+        break;
+        default:
+            alert("It's a yes or no question, dummy!");
+        break;
+    }
+
+function game () {
 //Ask user to choose rock, paper, or scissors
-let playerSelection = prompt("Choose: Rock, Paper, Scissors.", )
+let playerSelection = prompt("Choose: Rock, Paper, Scissors.", );
+
 switch (playerSelection.toLowerCase()) {
-    case "rock": 
+     case "rock": 
         console.log("You chose Rock!")
     break;
     case "paper":
@@ -32,31 +50,44 @@ let computerSelection = getComputerChoice();
 console.log("Computer chose " + computerSelection + "!");
 
 //Compare user choice against computer choice
-let playerScore = 0;
-let computerScore = 0;
-let tieString = "It's a tie! You both chose " + computerSelection + "!\nPlayer Score: " + playerScore + " | Computer Score: " + computerScore;
-let winString = "You Win! " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + " beats " + computerSelection + "!\nPlayer Score: " + playerScore++ + " | Computer Score: " + computerScore;
-let loseString = "You Lose! " + computerSelection + " beats " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + "!\n Player Score: " + playerScore + " | Computer Score: " + computerScore++;
-let invalid = "You made an invalid selection! Please refresh and choose Rock, Paper, or Scissors.";
-
 function playRound(playerSelection, computerSelection) {
+
+     
     if 
         (((playerSelection.toLowerCase() === 'rock') && computerSelection === 'Rock') ||
         ((playerSelection.toLowerCase() === 'paper') && computerSelection === 'Paper') ||
         ((playerSelection.toLowerCase() === 'scissors') && computerSelection === 'Scissors')) {
-            return tieString;
+            return tieString = "It's a tie! You both chose " + computerSelection + "!\nPlayer Score: " + playerScore + " | Computer Score: " + computerScore;
     } else if
         (((playerSelection.toLowerCase() === 'rock') && computerSelection === 'Paper') ||
         ((playerSelection.toLowerCase() === 'paper') && computerSelection === 'Scissors') ||
         ((playerSelection.toLowerCase() === 'scissors') && computerSelection === 'Rock')) { 
-            return loseString; 
+            computerScore = computerScore + 1;
+            return loseString = "You Lose! " + computerSelection + " beats " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + "!\n Player Score: " + playerScore + " | Computer Score: " + computerScore;;
+            
     } else if
         (((playerSelection.toLowerCase() === 'rock') && computerSelection === 'Scissors') ||
         ((playerSelection.toLowerCase() === 'paper') && computerSelection === 'Rock') ||
         ((playerSelection.toLowerCase() === 'scissors') && computerSelection === 'Paper')) { 
-            return winString;
+            playerScore = playerScore + 1;
+            return winString = "You Win! " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + " beats " + computerSelection + "!\nPlayer Score: " + playerScore + " | Computer Score: " + computerScore;
     } else 
-        return invalid;
+        return invalid = "You made an invalid selection! Please refresh and choose Rock, Paper, or Scissors.";
     };
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection,computerSelection));
+
+let playAgain = prompt("Play again?");
+
+switch (playAgain.toLowerCase()) {
+    case "yes": 
+        console.log(game());
+    break;
+    case "no":
+        alert("You're no fun!")
+    break;
+    default:
+        alert("It's a yes or no question, dummy!");
+    break;
+}
+}
